10db9174eb767b84d13e48a360388022ad491a6f
«lisa1am» <laitmouloud@gmail.com>
2020-04-27 12:32:47 +0200

    testing signature functions

--- signature.cpp
+++ signature.cpp
@@ -40,35 +40,31 @@ void substr(char s[], char sub[], int p, int l) {
 }
 
 
-void from_string(char str[], uint8_t arr[]) {
-
-  int cum = 1;
-  int step = 3;
-  int i;
-  for (i = 0; cum <= strlen(str); i++) {
-    char sub[step];
-    substr(str, sub,cum, step);
-    if(atoi(sub) > 255) {
-      substr(str, sub,cum, step-1);
-      cum += step - 1;
-    } else cum += step;
-    arr[i] = atoi(sub);
-    }
-}
-
- 
 void to_string(uint8_t arr[], char str[], int size) {
+ 
+ char* s_buff = (char*)malloc(sizeof(char)*2);
+ for (int i = 0; i < size; i++) {
+ sprintf(s_buff, "%x", arr[i]);
+ if(arr[i]>15) {
+ strcat(str, s_buff);
+ } else {
 
-  char* s_buff = (char*)malloc(sizeof(char)*3);
-  for (int i = 0; i < size; i++) {
-      if(arr[i] != 0) {
-        sprintf(s_buff, "%d", arr[i]);
-        strcat(str, s_buff);
-      }
-  }
+ strcat(str, "0");
+  strcat(str, s_buff);
+ }
+ }
 }
-
-
+void from_string(char str[], uint8_t arr[]) {
+ int cum = 1;
+ int step = 2;
+ int i;
+ for (i = 0; cum < strlen(str); i++) {
+ char sub[step];
+ substr(str, sub,cum, step);
+ cum +=step;
+ arr[i] = strtol(sub, NULL, 16);
+ }
+}
 void generatePairKey(char priv[], char pub[]){
 
 	#if uECC_SUPPORTS_secp256r1
@@ -79,6 +75,19 @@ void generatePairKey(char priv[], char pub[]){
 	uint8_t publicKeyInt[64];
 
 	uECC_make_key(publicKeyInt, privateKeyInt,curves[0]);
+
+	cout << "\nPUBLIC = \n";
+	for(int i=0; i<64; i++){
+		printf("%d,",publicKeyInt[i]);
+	}
+	cout << "\n";
+
+	cout << "\nPRIVATE = \n";
+	for(int i=0; i<32; i++){
+		printf("%d,",privateKeyInt[i]);
+	}
+	cout << "\n";
+
 
 	to_string(publicKeyInt, pub, 64);
 	to_string(privateKeyInt, priv, 32);
@@ -102,12 +111,12 @@ void sign(char priv[], char hash_code[], char sig[]){
 
 
 	to_string(sigInt, sig, 64);
-	std::cout << "\nSIGNATURE CHAR SIGN() = " << sig;
+	std::cout << "\nSIGNATURE CHAR SIGN() = \n" << sig;
 
 
-	cout << "\nSIGNATURE BASE 10 SIGN() = ";
+	cout << "\nSIGNATURE BASE 10 SIGN() = \n";
 	for(int i=0; i<64; i++){
-		printf("%d", sigInt[i]);
+		printf("%d,",sigInt[i]);
 	}
 	cout << "\n";
 }
@@ -126,22 +135,18 @@ bool verify(char pub[], char hash_code[], char sig[]){
 	from_string(hash_code, hashInt);
 	from_string(sig, sigInt);
 
-	std::cout << "\nSIGNATURE CHAR VERIFY() = " << sig;
+	std::cout << "\nSIGNATURE CHAR VERIFY() = \n" << sig;
 
-	cout << "\nSIGNATURE BASE 10 VERIFY() = ";
+	cout << "\nSIGNATURE BASE 10 VERIFY() = \n";
 	for(int i=0; i<64; i++){
-		printf("%d", sigInt[i]);
+		printf("%d,", sigInt[i]);
 	}
 	cout << "\n";
 
 
+	uECC_verify(publicKeyInt, hashInt, sizeof(hashInt), sigInt, curves[0]);
 	if(uECC_verify(publicKeyInt, hashInt, sizeof(hashInt), sigInt, curves[0])){
-
-		cout << "\nSIG INT = ";
-		vli_print(sigInt, sizeof(sigInt));
-
-		to_string(sigInt, sig, 64);
-
+		cout << "\nVERIFIED!";
 		return true;
 	}
 	else{
@@ -151,17 +156,21 @@ bool verify(char pub[], char hash_code[], char sig[]){
 
 
 int main(){
-	char priv[100], pub[100], hash_code[100], sig[100];
+	char priv[500], pub[500], hash_code[500], sig[500], sigTest[500];
 	uint8_t sigInt[64];
 
 	generatePairKey(priv, pub);
+	cout << "-------------->\n" << priv << "\n" << pub;
 	sign(priv, hash_code, sig);
-	std::cout << "\nSIGNATURE MAIN = " << sig;
+	std::cout << "\nSIGNATURE MAIN = \n" << sig;
 	from_string(sig, sigInt);
-	cout << "\nSIGNATURE BASE 10 MAIN = ";
+	cout << "\nSIGNATURE BASE 10 MAIN = \n";
 	for(int i=0; i<64; i++){
-		printf("%d", sigInt[i]);
+		printf("%d,", sigInt[i]);
 	}
+	
+	to_string(sigInt, sigTest, 64);
+	cout << "\nSIGNATURE APRES TO_STRING MAIN = \n" << sigTest;
 	cout << "\n";
 	std::cout <<  verify(pub, hash_code,sig) << "\n";
 
