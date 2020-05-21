d8af777e16b3fdfbae79eb61bf5a185af78088a9
«lisa1am» <laitmouloud@gmail.com>
2020-05-21 05:20:13 +0200

    added flags in make file

--- gcc.mk
+++ gcc.mk
@@ -2,13 +2,11 @@
 	gcc -fPIC ${CFLAGS} -c $< -o $@
 
 %.o : %.cpp
-	#g++ -fPIC ${CPPFLAGS} $< ${CRYPTOLIB}.c ${CRYPTOLIB}.h -o $@
-	#g++ -fPIC ${CPPFLAGS} ${CRYPTOLIB}.c ${CRYPTOLIB}.h -c $< -o $@
-	g++ -fPIC ${CPPFLAGS} -c $< -o $@
+	g++ -fPIC ${CPPFLAGS} -c $< -o  $@
 
 
 ${EXE} : ${OBJS}
-	g++ ${OBJS} ${LDFLAGS} -o $@
+	g++ ${OBJS} ${LDFLAGS} -o -lboost_python -lboost_system -lpython2\.7$@
 
 ${LIBSO} : ${LIBSO_OBJS}
 	gcc -o $@ -shared ${LDFLAGS} $^ ${LIBS}
