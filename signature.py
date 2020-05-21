71a6818e2e3f7533e7eb37fff6843bb59f9b1c22
«lisa1am» <laitmouloud@gmail.com>
2020-05-21 05:38:34 +0200

    test code python

--- signature.py
+++ signature.py
@@ -1,8 +1,8 @@
 import signature
 
-def generatePairKey():
-	val=signature.generatePairKey()
-	return val
-
-
-print signature.generatePairKey();
+keys=signature.generatePairKey();
+priv=split(keys,1);
+pub = signature.split(keys,0);
+signature.sign(priv,"5a822196bf1c45b1e74c6d04e4127d0296d64695932f40909bdab3ba8a9b0528");
+bool = signature.verify(pub, "5a822196bf1c45b1e74c6d04e4127d0296d64695932f40909bdab3ba8a9b0528", sig);
+print(bool);
